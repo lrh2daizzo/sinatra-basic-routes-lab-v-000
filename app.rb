a3de8d4bb2ccdb0dcc395c0c8b4e28e4738a7133
Luis Herrera <herrera.lrh@gmail.com>
2020-11-12 02:11:04 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -2,15 +2,5 @@ require 'sinatra'
 
  class App < Sinatra::Base
 
-   get '/name' do
-     "My name is Luis."
-   end
 
-   get '/hometown' do
-     "My hometown is Dallas."
-   end
-
-   get '/favorite-song' do
-     'My favorite song is Runaway by Kanye West.'
-   end
  end
